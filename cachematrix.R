da7d79551b26018bfa350f30becf3c069ba165b1
juan <51038935+juan12342@users.noreply.github.com>
2020-06-28 02:00:25 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+## Esta función calcula el inverso de la "matriz" especial 
+##devuelta por makeCacheMatrix arriba. Si el inverso ya se ha 
+##calculado (y la matriz no ha cambiado),
+##entonces el caché debe recuperar el inverso del caché. 
+##Calcular el inverso de una matriz cuadrada se puede hacer 
+##con la función resolver en R. Por ejemplo, si X es una matriz invertible cuadrada, resolver (X) devuelve su inverso.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached result")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
